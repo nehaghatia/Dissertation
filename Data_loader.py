638e70ada5a0941cc6fc294ce91891742fb9cbb5
Neha Ghatia <nehaghatia3@gmail.com>
2020-06-24 10:58:04 +0100

    added imports

--- Data_loader.py
+++ Data_loader.py
@@ -1,1 +1,13 @@
-Hello
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+HGG_path = "C:/Data/BraTS18Train/HGG"
+# bratslgg_path = "D:\Data\\brats18\LGG"
+flair_name = "_flair.nii.gz"
+t1_name = "_t1.nii.gz"
+t1ce_name = "_t1ce.nii.gz"
+t2_name = "_t2.nii.gz"
+mask_name = "_seg.nii.gz"
